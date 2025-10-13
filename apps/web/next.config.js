2792c765553bc89a57304c2bc79d126862708f3c
David Maman <davidmaman8@icloud.com>
2025-10-13 15:58:57 -0400

    feat: brief description of changes

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,7 +1,18 @@
+import path from 'path';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    forceSwcTransforms: true
-  }
+    forceSwcTransforms: true,
+  },
+  transpilePackages: ['@cliply/shared'],
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      '@shared': path.resolve('./packages/shared/src'),
+    };
+    return config;
+  },
 };
+
 export default nextConfig;
